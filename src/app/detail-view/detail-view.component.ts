0fc83d1bc6635d46a0fabd3323ac54aadf229314
HansStieber <hans.stieber@posteo.de>
2023-05-01 16:37:16 +0200

    commenting finished at detail view component

--- src/app/detail-view/detail-view.component.ts
+++ src/app/detail-view/detail-view.component.ts
@@ -29,15 +29,21 @@ export class DetailViewComponent implements OnInit {
   constructor(
     private dialogRef: MatDialogRef<DetailViewComponent>,
     private http: HttpClient,
-    private data: PokemonService,
+    private pokemonData: PokemonService,
     private favoritesData: FavoritesService,
     private router: Router,
     private sanitizer: DomSanitizer
   ) { }
 
 
+  /**
+   * This function runs when the component gets initialized. It subscribes to the pokemon and favorites services, which
+   * provide all currently loaded pokemon and current favorite pokemon. It also sets detail variables that provide
+   * information about the pokemon that is currently opened in detail view. It also checks if the current pokemon is
+   * already added to the favorites list.
+   */
   ngOnInit(): void {
-    this.data.pokemon.subscribe(pokemon => {
+    this.pokemonData.pokemon.subscribe(pokemon => {
       this.allPokemon = pokemon;
     });
     this.favoritesData.favorites.subscribe(favorites => {
@@ -48,6 +54,18 @@ export class DetailViewComponent implements OnInit {
   }
 
 
+  /**
+   * This function Capitalizes a word with the CapitalizeService.
+   */
+  capitalizeWord(word: string) {
+    const capitalizeService = new CapitalizeService();
+    return capitalizeService.capitalizedWord(word);
+  }
+
+
+  /**
+   * This function sets the detail variables of the pokemon that is currently viewed in detail view.
+   */
   getDetails() {
     this.types = this.details.types[0];
     this.type = this.types['type']['name'];
@@ -58,79 +76,122 @@ export class DetailViewComponent implements OnInit {
   }
 
 
-  capitalizeWord(word: string) {
-    const capitalizeService = new CapitalizeService();
-    return capitalizeService.capitalizedWord(word);
-  }
-
+  /**
+   * When the user is in detail view, this function selects the next Pokemon either from the allPokemon array or the
+   * favorites array.
+   */
   nextPokemon() {
     if (this.router.url === '/pokemon' || this.router.url === '/') {
-      let index = this.details.id;
-      if (index > this.allPokemon.length - 1) {
-        index = 0;
-      }
-      let newPokemon = new Pokemon(this.allPokemon[index]);
-      this.http
-        .get<any>(newPokemon.url)
-        .subscribe(data => {
-          this.details = new Details(data);
-          this.getDetails();
-          this.checkIfPokemonIsFavorite();
-        });
+      this.getNextPokemonFromAllPokemon();
     }
     if (this.router.url === '/favorites') {
-      this.favorites.forEach(pokemon => {
-        if (pokemon.name === this.details.name) {
-          let index = this.favorites.indexOf(pokemon) + 1;
-          if (index > this.favorites.length - 1) {
-            index = 0;
-          }
-          let newPokemon = new Pokemon(this.favorites[index]);
-          this.http
-            .get<any>(newPokemon.url)
-            .subscribe(data => {
-              this.details = new Details(data);
-              this.getDetails();
-              this.checkIfPokemonIsFavorite();
-            });
-        }
-      });
+      this.getNextPokemonFromFavorites();
     }
   }
 
+
+  /**
+   * This function selects the next pokemon from the allPokemon array. The index of the next pokemon equals the value
+   * of the id of the current pokemon. If the next Pokemon has not been loaded yet, the first pokemon of the allPokemon
+   * array is selected. The new Pokemon is subscribed to get the details of the new/next Pokemon.
+   */
+  getNextPokemonFromAllPokemon() {
+    let index = this.details.id;
+    if (index > this.allPokemon.length - 1) {
+      index = 0;
+    }
+    let newPokemon = new Pokemon(this.allPokemon[index]);
+    this.subscribeToNewPokemon(newPokemon);
+  }
+
+
+  /**
+   * This function selects the next Pokemon from the favorites array. This is achieved by searching the array for the name
+   * of the current pokemon, getting the index of that pokemon at the favorites array and adding plus one to it. The new
+   * index is used to get the next pokemon from the favorites array. The new Pokemon is subscribed to get the details of
+   * the new/next Pokemon.
+   */
+  getNextPokemonFromFavorites() {
+    this.favorites.forEach(pokemon => {
+      if (pokemon.name === this.details.name) {
+        let index = this.favorites.indexOf(pokemon) + 1;
+        if (index > this.favorites.length - 1) {
+          index = 0;
+        }
+        let newPokemon = new Pokemon(this.favorites[index]);
+        this.subscribeToNewPokemon(newPokemon);
+      }
+    });
+  }
+
+
+  /**
+   * When the user is in detail view, this function selects the previous Pokemon either from the allPokemon array or the
+   * favorites array.
+   */
   previousPokemon() {
     if (this.router.url === '/pokemon' || this.router.url === '/') {
-      let newPokemon = new Pokemon(this.allPokemon[this.details.id - 2]);
-      this.http
-        .get<any>(newPokemon.url)
-        .subscribe(data => {
-          this.details = new Details(data);
-          this.getDetails();
-          this.checkIfPokemonIsFavorite();
-        });
+      this.getPreviousPokemonFromAllPokemon();
     }
     if (this.router.url === '/favorites') {
-      this.favorites.forEach(pokemon => {
-        if (pokemon.name === this.details.name) {
-          let index = this.favorites.indexOf(pokemon) - 1;
-          if (index < 0) {
-            index = this.favorites.length - 1;
-          }
-          let newPokemon = new Pokemon(this.favorites[index]);
-          this.http
-            .get<any>(newPokemon.url)
-            .subscribe(data => {
-              this.details = new Details(data);
-              this.getDetails();
-              this.checkIfPokemonIsFavorite();
-            });
-        }
-      });
+      this.getPreviousPokemonFromFavorites();
     }
   }
 
+
+  /**
+  * This function selects the previous pokemon from the allPokemon array. The index of the next pokemon equals the value
+  * of the id of the current pokemon minus 2. The new Pokemon is subscribed to get the details of the new/previous Pokemon.
+  */
+  getPreviousPokemonFromAllPokemon() {
+    let newPokemon = new Pokemon(this.allPokemon[this.details.id - 2]);
+    this.subscribeToNewPokemon(newPokemon);
+  }
+
+
+    /**
+   * This function selects the previous Pokemon from the favorites array. This is achieved by searching the array for the name
+   * of the current pokemon, getting the index of that pokemon at the favorites array and subtracting 1 of it. The new
+   * index is used to get the next pokemon from the favorites array. If the new index is lower than 0, the last pokemon
+   * of the array is picked. The new Pokemon is subscribed to get the details of the new/previous Pokemon.
+   */
+  getPreviousPokemonFromFavorites() {
+    this.favorites.forEach(pokemon => {
+      if (pokemon.name === this.details.name) {
+        let index = this.favorites.indexOf(pokemon) - 1;
+        if (index < 0) {
+          index = this.favorites.length - 1;
+        }
+        let newPokemon = new Pokemon(this.favorites[index]);
+        this.subscribeToNewPokemon(newPokemon);
+      }
+    });
+  }
+
+
+  /**
+   * This function subscribes to the new Pokemon when you select a previous or the next pokemon of your array. It sets
+   * the details accordingly and checks if the Pokemon is already a favorite.
+   */
+  subscribeToNewPokemon(newPokemon: Pokemon) {
+    this.http
+      .get<any>(newPokemon.url)
+      .subscribe(data => {
+        this.details = new Details(data);
+        this.getDetails();
+        this.checkIfPokemonIsFavorite();
+      });
+  }
+
+
+  /**
+   * When a key is pressed, a defined event is triggered.
+   */
   @HostListener('document:keydown', ['$event'])
 
+  /**
+   * The function chekcs which key is currently pressed, and selects functions to run accordingly.
+   */
   handleKeyEvent(event: KeyboardEvent) {
     if (event.key === 'ArrowLeft') {
       this.previousPokemon();
@@ -141,6 +202,12 @@ export class DetailViewComponent implements OnInit {
   }
 
 
+  /**
+   * The function sets the newFavoritePokemon variable to the currently selected Pokemon. It then checks if the current
+   * Pokemon is already part of the favorites array. If the pokemon isn´t part, it is added to the global favorites array
+   * by using the addFavorites function of the favorites service. The alreadyFavorite variable is set to true (heart icon
+   * at detail card changes) and the favorites array is saved at the localStorage.
+   */
   addToFavorites() {
     let newFavoritePokemon = new Pokemon(this.allPokemon[this.details.id - 1]);
     if (!this.favorites.some(pokemon => pokemon.name === newFavoritePokemon.name)) {
@@ -149,10 +216,15 @@ export class DetailViewComponent implements OnInit {
       let serializedArray = JSON.stringify(this.favorites);
       localStorage.setItem('favorite-pokemon', serializedArray);
     }
-
   }
 
 
+  /**
+   * The function selects the next Pokemon of the favorites array, before it removes the current pokemon from the
+   * favorites array by using the removeFavorites function of the favorites service. It sets the alreadyFavorite variable
+   * to false (the heart icon at the details variable changes) and the favorites array is saved at the localStorage.
+   * If the favorites array is empty, the detail view dialog is closed.
+   */
   removeFromFavorites() {
     this.nextPokemon();
     this.favoritesData.removeFavorites(this.details);
@@ -165,6 +237,11 @@ export class DetailViewComponent implements OnInit {
   }
 
 
+  /**
+   * The Function checks if the current Pokemon is already a favorite by checking if some Pokemon of the favorites array
+   * has the same name. If it is already favorite the alreadyFavorite variable is set to true to change the favorite symbol
+   * at the detail view dialog. If it is no favorite it sets the alreadyFavorite variable to false.
+   */
   checkIfPokemonIsFavorite() {
     if (this.favorites && this.favorites.some(pokemon => pokemon.name === this.details.name)) {
       this.alreadyFavorite = true;
@@ -174,7 +251,13 @@ export class DetailViewComponent implements OnInit {
   }
 
 
+  /**
+   * The function sanitizes a selected image.
+   * 
+   * @param imageUrl - Image Url that is to be sanitized
+   * @returns Sanitized Url
+   */
   sanitizeImageUrl(imageUrl: string): SafeUrl {
     return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
-}
+  }
 }
